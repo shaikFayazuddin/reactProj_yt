771852b708feab29f5ce98d62a3d5bf604a6d44a
shaikFayazuddin <shaikfayazuddin18@gmail.com>
2024-05-30 12:21:10 +0530

    made changes

---
+++ project2/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Navigation/Navigation'
+import ContactHeader from './components/contactHeader/contactHeader'
+
+function App() {
+
+  return (
+    <div>
+      <Header></Header>
+      <ContactHeader></ContactHeader>
+    </div>
+  )
+}
+
+export default App
