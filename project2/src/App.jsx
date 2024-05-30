aba5982d65c2702e736e9a9907c08649019ca095
shaikFayazuddin <shaikfayazuddin18@gmail.com>
2024-05-30 12:40:26 +0530

    done with header

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Header from './components/Navigation/Navigation'
-import ContactHeader from './components/contactHeader/contactHeader'
+import ContactHeader from './components/ContactHeader/ContactHeader'
 
 function App() {
 
