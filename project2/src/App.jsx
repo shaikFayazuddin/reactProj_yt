b5762e41a900eec843e0fd9dd41f36b3ba782063
shaikFayazuddin <shaikfayazuddin18@gmail.com>
2024-06-07 11:12:58 +0530

    project complete almost

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -2,14 +2,15 @@ import './App.css'
 import Header from './components/Navigation/Navigation'
 import ContactHeader from './components/ContactHeader/ContactHeader'
 import ContactForm from './components/ContactForm/ContactForm'
-
 function App() {
 
   return (
     <div>
       <Header></Header>
+      <main className="main_container">
       <ContactHeader></ContactHeader>
       <ContactForm></ContactForm>
+      </main>
     </div>
   )
 }
--- project2/src/components/ContactForm/ContactForm.jsx
+++ project2/src/components/ContactForm/ContactForm.jsx
@@ -1,7 +1,8 @@
 import styles from "./ContactForm.module.css";
 import Button from "../Button/Button";
 import { MdMessage } from "react-icons/md";
-import {FaPhoneAlt} from "react-icons/fa"
+import { FaPhoneAlt } from "react-icons/fa";
+import { HiMail } from "react-icons/hi";
 
 const ContactForm = () => {
   return (
@@ -13,10 +14,41 @@ const ContactForm = () => {
             text="VIA SUPPORT CHAT"
           />
           <Button 
-          icon={<FaPhoneAlt fontSize="24px" />} text="hello"></Button>
+          icon={<FaPhoneAlt fontSize="24px" />} 
+          text="hello" />
         </div>
+        <Button
+          isOutline={true}
+          icon={<HiMail fontSize="24px" />} 
+          text="VIA EMAIL FORM" />
+
+      <form>
+        <div className={styles.form_control}>
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name"/>
+        </div>
+
+        <div className={styles.form_control}>
+          <label htmlFor="name">Email</label>
+          <input type="text" name="email" />
+        </div>
+
+        <div className={styles.form_control}>
+          <label htmlFor="name">Text</label>
+          <textarea name="text" id="text" rows="8"></textarea>
+        </div>
+
+        <div className={styles.submit_button}>
+          <Button text="VIA SUPPORT CHAT"/>
+        </div>
+
+      </form>
       </div>
-      <div className={styles.contact_image}></div>
+
+
+      <div className={styles.contact_image}>
+        <img src="../../../public/images/Service 24_7-pana 1.png" alt="form_image" />
+      </div>
     </section>
   );
 };
